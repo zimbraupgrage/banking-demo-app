8a790e7b9fa67e9513dc577c22eb7441f09ec0b6
zimbraupgrage <you@example.com>
2026-04-26 13:09:20 +0530

    fix server listen for render

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.get("/transactions", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port", PORT);
 });
